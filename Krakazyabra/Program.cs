3411236bfe8e820b3b90ce8824fd23c22da01654
Kadushkin Konstantin <aveko7@ya.ru>
2014-09-22 23:38:18 +0400

    Update Program.cs

--- Krakazyabra/Program.cs
+++ Krakazyabra/Program.cs
@@ -25,8 +25,33 @@ namespace Krakazyabra
 	class MainClass
 	{
 		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello, dear Krakazyabra");
+		{	
+			Console.WriteLine ("Поочередно введите значения, перед (1)x^2, перед (2)x, свободный коээфициент (3)");
+			double a = Convert.ToDouble(Console.ReadLine());
+			double b = Convert.ToDouble(Console.ReadLine());
+			double c = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Ваше уравнение {0}x^2+{1}x+{2}=0",a,b,c);
+			double d = b * b - 4 * a * c;
+			double o = 0;
+			if (d < o) {
+				Console.WriteLine ("d={0} Натуральных корней нет", d);
+				Console.ReadKey(); }
+			if (d > o) {
+				double kd = Math.Sqrt(d);
+				double x1 = -b + kd / 2 * a;
+				double x2 = -b - kd / 2 * a;
+				Console.WriteLine ("Уравнение имеет два корня");
+				Console.WriteLine ("x1 = {0} + {1} / {2}", -b, kd, 2*a);
+				Console.WriteLine ("x1 = {0}",x1);
+				Console.WriteLine ("x2 = {0} - {1} / {2}", -b, kd, 2*a);
+				Console.WriteLine ("x2 = {0}",x2);
+				Console.ReadKey(); }
+			else {
+				double x = -b / 2 * a;
+				Console.WriteLine ("Уравнение имеет единственный корень");
+				Console.WriteLine("x = {0} / {1}",-b,2*a);
+				Console.WriteLine ("x = {0}", x);
+				Console.ReadKey(); }
 		}
 	}
 }
